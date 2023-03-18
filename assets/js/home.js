e839d6a106eb89cfcf27beff117f9c2ae0d57372
AfandiyevMusa <113282393+AfandiyevMusa@users.noreply.github.com>
2023-03-19 02:46:39 +0400

    navbar almost ready

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,23 @@
-$(function(){
+$(function () {
+
+    let namesInSidebar = document.querySelectorAll(".for-names .names h6")
+    let namesElems = document.querySelectorAll(".infos .elems")
+
+    namesInSidebar.forEach(eachName => {
+        eachName.addEventListener("click", function (e) {
+            document.querySelector(".active-menu").classList.remove("active-menu");
+            this.classList.add("active-menu");
+
+            namesElems.forEach(eachElem => {
+                if (this.getAttribute("data-id") == eachElem.getAttribute("data-id")) {
+                    eachElem.classList.remove("d-none");
+                } else {
+                    eachElem.classList.add("d-none");
+                }
+            });
+
+
+        })
+    });
 
 })
