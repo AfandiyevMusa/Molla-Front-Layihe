4e4e5f8e942c5822ea540ac1f52cd0f634825bac
AfandiyevMusa <113282393+AfandiyevMusa@users.noreply.github.com>
2023-03-21 18:33:53 +0400

    wishlist and cart pages are done

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,4 +1,38 @@
 $(function(){
+    //Sidebar
+    let sidebar = document.querySelector(".sidebar");
+    let closeButton = document.querySelector(".sidebar .close-part .close");
+    let openButton = document.querySelector("#down-nav-phone .container .logo i")
+
+    openButton.addEventListener("click", function () {
+        sidebar.classList.add("active-bar");
+        sidebar.classList.remove("hide");
+    })
+
+    closeButton.addEventListener("click", function () {
+        sidebar.classList.add("hide");
+        sidebar.classList.remove("active-bar");
+    })
+
+    //Sidebar TabMenu
+    let namesInSidebar = document.querySelectorAll(".for-names .names h6")
+    let namesElems = document.querySelectorAll(".infos .elems")
+
+    namesInSidebar.forEach(eachName => {
+        eachName.addEventListener("click", function (e) {
+            document.querySelector(".active-menu").classList.remove("active-menu");
+            this.classList.add("active-menu");
+
+            namesElems.forEach(eachElem => {
+                if (this.getAttribute("data-id") == eachElem.getAttribute("data-id")) {
+                    eachElem.classList.remove("d-none");
+                } else {
+                    eachElem.classList.add("d-none");
+                }
+            });
+        })
+    });
+    
     //Login TabMenu
     let choices = document.querySelectorAll("#register .all .register-part .upregister .tab-menu .for-names .names h4");
     let infos = document.querySelectorAll("#register .all .register-part .upregister .tab-menu .infos .elems");
--- assets/js/home.js
+++ assets/js/home.js
@@ -142,17 +142,41 @@ $(function () {
                 items: 1,
                 nav: true
             },
-            600: {
-                items: 3,
+            500: {
+                items: 1,
                 nav: false
             },
-            1000: {
-                items: 5,
-                nav: true,
-                loop: true
+            900: {
+                items: 2,
+                nav: true
+            },
+            1200: {
+                items: 4,
+                nav: false,
+                loop: false
             }
         }
     })
+    // $('.allbrands').owlCarousel({
+    //     loop: true,
+    //     margin: 10,
+    //     responsiveClass: true,
+    //     responsive: {
+    //         0: {
+    //             items: 1,
+    //             nav: true
+    //         },
+    //         600: {
+    //             items: 3,
+    //             nav: false
+    //         },
+    //         1000: {
+    //             items: 5,
+    //             nav: true,
+    //             loop: true
+    //         }
+    //     }
+    // })
 
     //Trending Products Carousel
     // $('.owl-carousel').owlCarousel({
